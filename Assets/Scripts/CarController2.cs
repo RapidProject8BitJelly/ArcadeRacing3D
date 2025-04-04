dca5b584fc61a353e3060d6e9d1370927108900e
Olga Maruszyńska <olga.maruszynska@gmail.com>
2025-04-04 21:41:29 +0200

    Added drift system

--- Assets/Scripts/CarController2.cs
+++ Assets/Scripts/CarController2.cs
@@ -6,7 +6,10 @@ public class CarController2 : MonoBehaviour
     [SerializeField] private float acceleration;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float turnFactor;
+    [SerializeField] private float driftFactor;
     [SerializeField] private TMP_Text speedText;
+    [SerializeField] private TrailRenderer[] trailsRenderer;
+    [SerializeField] private ParticleSystem[] particleSystem;
 
     private float _accelerationInput;
     private float _turnInput;
@@ -23,7 +26,15 @@ public class CarController2 : MonoBehaviour
         _accelerationInput = Input.GetAxis("Vertical");
         _turnInput = Input.GetAxis("Horizontal");
         AddSpeed();
+        Drift();
         Turn();
+        
+        float lateralVelocity;
+        bool isBraking;
+        bool isScreeching = IsTireScreeching(out lateralVelocity, out isBraking);
+
+        SetTrailsRenderers(isScreeching);
+        
 
         float speed = _rigidbody.velocity.magnitude * 3.6f;
         speedText.SetText(Mathf.RoundToInt(speed).ToString());
@@ -54,5 +65,57 @@ public class CarController2 : MonoBehaviour
         _rotationAngle -= _turnInput * turnFactor * minSpeedBeforeAllowTurningFactor;
         Quaternion deltaRotation = Quaternion.Euler(0f, -_rotationAngle, 0f);
         _rigidbody.MoveRotation(deltaRotation);
+    }
+    private void Drift()
+    {
+        Vector3 forwardVelocity = transform.forward * Vector3.Dot(_rigidbody.velocity, transform.forward);
+        Vector3 rightVelocity = transform.right * Vector3.Dot(_rigidbody.velocity, transform.right);
+
+        _rigidbody.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+    private void SetTrailsRenderers(bool screeching)
+    {
+        foreach (var trail in trailsRenderer)
+        {
+            if (trail != null)
+            {
+                trail.emitting = screeching;
+            }
+        }
+        
+        foreach (var particle in particleSystem)
+        {
+            if (particle != null)
+            {
+                if (screeching)
+                {
+                    particle.Play();
+                }
+                else
+                {
+                    particle.Stop();
+                }
+            }
+        }
+    }
+
+    private bool IsTireScreeching(out float lateralVelocity, out bool isBraking)
+    {
+        lateralVelocity = Vector3.Dot(_rigidbody.velocity, transform.right);
+        isBraking = false;
+
+        if (_accelerationInput < 0 && Vector3.Dot(_rigidbody.velocity, transform.forward) > 0f)
+        {
+            isBraking = true;
+            return true;
+        }
+
+        if (Mathf.Abs(lateralVelocity) > 3f)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
