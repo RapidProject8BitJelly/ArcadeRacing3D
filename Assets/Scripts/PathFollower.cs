20007b6ff662985cd88f6dbad97c97ac7aaa77a6
Olga Maruszyńska <olga.maruszynska@gmail.com>
2025-05-16 10:22:40 +0200

    Better path follower

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -7,6 +7,8 @@ using DG.Tweening;
 public class PathFollower : MonoBehaviour
 {
     [SerializeField] private GameObject path;
+    [SerializeField] private float speed;
+    public bool isMoving;
 
     private void Start()
     {
@@ -15,12 +17,35 @@ public class PathFollower : MonoBehaviour
 
     private IEnumerator FollowPathCoroutine()
     {
+        isMoving = true;
         for (int i = 0; i < path.transform.childCount; i++)
         {
-            transform.DOLookAt(path.transform.GetChild(i).position, 0.5f, AxisConstraint.Y, Vector3.up).SetEase(Ease.Linear);
-            yield return transform.DOMove(path.transform.GetChild(i).position, 0.5f).SetEase(Ease.Linear)
-                .WaitForCompletion();
+            Vector3 currentTarget = path.transform.GetChild(i).position;
+            float distance = Vector3.Distance(transform.position, currentTarget);
+            float duration = distance / speed;
+            
+            Vector3 forwardDir;
+
+            if (i < path.transform.childCount - 1)
+            {
+                Vector3 next = path.transform.GetChild(i + 1).position;
+                forwardDir = (next - transform.position).normalized;
+            }
+            else
+            {
+                forwardDir = (currentTarget - transform.position).normalized;
+            }
+            
+            forwardDir.y = 0;
+            
+            if (forwardDir != Vector3.zero)
+            {
+                Quaternion targetRot = Quaternion.LookRotation(forwardDir, Vector3.up);
+                transform.DORotateQuaternion(targetRot, duration).SetEase(Ease.Linear);
+            }
+            
+            yield return transform.DOMove(currentTarget, duration).SetEase(Ease.Linear).WaitForCompletion();
         }
+        isMoving = false;
     }
-    
 }
