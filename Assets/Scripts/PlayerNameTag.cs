081c5a28789c5bf9cf5e2d1e71d9c7d01f16ce5f
Olga Maruszyńska <olga.maruszynska@gmail.com>
2025-05-26 20:57:33 +0200

    Added player nickname tags

---
+++ Assets/Scripts/PlayerNameTag.cs
@@ -0,0 +1,25 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameTag : MonoBehaviour
+{
+    [SerializeField] private GameObject playerNicknameBackground;
+    [SerializeField] private TMP_Text playerNameText;
+    [SerializeField] private PlayerCarSettings playerCarSettings;
+
+    private void Start()
+    {
+        playerNameText.text = playerCarSettings.nickname;
+        playerNameText.ForceMeshUpdate();
+        var scale = playerNicknameBackground.transform.localScale;
+        scale.x = (playerNameText.textBounds.size.x) + 0.4f;
+        playerNicknameBackground.transform.localScale = scale;
+    }
+
+    private void LateUpdate()
+    {
+        gameObject.transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, -90, 0);
+    }
+}
