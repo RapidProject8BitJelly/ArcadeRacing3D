5bf6c2cd2c5b43d9c8182993eacf9282399b4bc9
Jessi <jessica.szarek25@gmail.com>
2025-06-05 19:00:38 +0200

    menu highlight effect

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MenuButton : Button
+public class MenuButton : Button, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private List<Image> _indicators = new List<Image>();
+    
     private TMP_Text _buttonText;
+    private Color32 _indicatorsColor = new Color32(255, 255, 255, 255);
+    private Color32 _hiddenIndicatorsColor = new Color32(255, 255, 255, 0);
     public TMP_Text ButtonText
     {
         get
@@ -14,5 +22,30 @@ public class MenuButton : Button
             }
             return _buttonText;
         }
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach (var indicator in _indicators)
+        {
+            indicator.color = _indicatorsColor;
+        }
+    }
+    
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (var indicator in _indicators)
+        {
+            indicator.color = _hiddenIndicatorsColor;
+        }
+    }
+}
+
+[CustomEditor(typeof(MenuButton))]
+public class MenuButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
     }
 }
