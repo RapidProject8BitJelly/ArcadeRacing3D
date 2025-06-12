bc684f7662edd2e89991a61b898f55d76b900bc2
Jessi <jessica.szarek25@gmail.com>
2025-06-12 22:48:52 +0200

    menu button action implemented

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -8,6 +8,15 @@ using UnityEngine.UI;
 public class MenuButton : Button, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private List<Image> _indicators = new List<Image>();
+    [SerializeField] private ActionType _actionType = ActionType.None;
+    
+    private enum ActionType
+    {
+        None,
+        StartGame,
+        Options,
+        Exit
+    }
     
     private TMP_Text _buttonText;
     
@@ -53,6 +62,40 @@ public class MenuButton : Button, IPointerEnterHandler, IPointerExitHandler
         if (ButtonText)
         {
             ButtonText.fontSize = BaseTextSize;
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        onClick.AddListener(Submit);
+    }
+    
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        onClick.RemoveListener(Submit);
+    }
+
+    private void Submit()
+    {
+        switch (_actionType)
+        {
+            case ActionType.StartGame:
+                // Load the game scene or start the game logic
+                break;
+            case ActionType.Options:
+                // Open options menu or settings
+                break;
+            case ActionType.Exit:
+                #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+                #endif
+                Application.Quit();
+                break;
+            default:
+                Debug.LogWarning("No action assigned to this button.");
+                break;
         }
     }
 }
