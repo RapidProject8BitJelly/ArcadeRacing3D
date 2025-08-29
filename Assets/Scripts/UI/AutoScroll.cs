c9da3164c9d732382db17ecb8b9fcddce84ea01f
Olga Maruszyńska <olga.maruszynska@gmail.com>
2025-08-29 18:31:38 +0200

    Add credits

---
+++ Assets/Scripts/UI/AutoScroll.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoScroll : MonoBehaviour
+{
+    [SerializeField] private Button _creditsButton;
+    [SerializeField] private Button _backButton;
+    [SerializeField] private GameObject _creditsContent;
+    [SerializeField] private float _scrollDuration;
+
+    private Vector2 _startPosition;
+    private Tween _creditsTween;
+    private RectTransform _creditsRectTransform;
+
+    private void Awake()
+    {
+        _startPosition = _creditsContent.transform.localPosition;
+        _creditsRectTransform = _creditsContent.GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        _creditsButton.onClick.AddListener(StartScrollCredits);
+        _backButton.onClick.AddListener(ResetCreditsPosition);
+    }
+
+    private void StartScrollCredits()
+    {
+        _creditsTween = _creditsContent.transform.DOLocalMoveY(_creditsRectTransform.sizeDelta.y * 2.5f, _scrollDuration).SetEase(Ease.Linear);
+    }
+
+    private void ResetCreditsPosition()
+    {
+        if (_creditsTween != null)
+        {
+            _creditsTween.Kill();
+            _creditsTween = null;
+        }
+
+        Vector3 pos = _creditsContent.transform.localPosition;
+        pos.y = _startPosition.y;
+        _creditsContent.transform.localPosition = pos;
+    }
+}
